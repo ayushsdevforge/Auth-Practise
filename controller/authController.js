ab19375114a6d04502839e05e1f43c1c273e96df
Ayush Kumar <ayush805126ind@gmail.com>
2026-01-08 23:06:52 +0530

    login  user feature is added

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,6 @@
 import userSchema from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
 
@@ -12,15 +14,42 @@ export const registerUser = async (req, res) => {
             return res.status(400).json({ message: 'User already exists' });
         }
 
-        const neewUser = new userSchema({
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new userSchema({
             name,
             email,
-            password
+            password: hashedPassword
         });
-        await neewUser.save();
+        await newUser.save();
         res.status(201).json({ message: 'User registered successfully' });      
     }
      catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+}
+
+export const loginUser = async(req, res) => {
+    try{
+
+       const {email, password} = req.body;
+
+       const user = await userSchema.findOne({ email});
+
+       if(!user){
+        return res.status(400).json({ message: 'Invalid credentials' });
+       }
+
+       const isMatch = await bcrypt.compare(password, user.password);
+
+       if(!isMatch){
+        return res.status(400).json({ message: 'Invalid credentials' });
+       }
+
+       res.status(200).json({ message: 'Login successful' });
+
+    }
+    catch (error) {
         res.status(500).json({ message: 'Server Error' });
     }
 }
