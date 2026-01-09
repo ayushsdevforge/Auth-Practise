c4e45d18d321f4238e31d9c835488ac8c6e3d8fe
Ayush Kumar <ayush805126ind@gmail.com>
2026-01-09 22:41:30 +0530

    fixed all errors

--- model/userModel.js
+++ model/userModel.js
@@ -14,6 +14,14 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true
+    },
+    resetPasswordOTP: {
+      type: String,
+      default: null
+    },
+    resetPasswordOTPExpiry: {
+      type: Date,
+      default: null
     }
   },
   {
